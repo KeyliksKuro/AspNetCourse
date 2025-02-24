edfec5104327ee9b4ca20eae34477165870af12a
Keyliks <115bsergey@gmail.com>
2025-02-24 14:14:43 +0700

    Добавьте файлы проекта.

---
+++ Lesson1/middleware/TimeMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace Lesson1.middleware
+{
+    public class TimeMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public TimeMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Path == "/time")
+            {
+                await context.Response.WriteAsync(DateTime.Now.ToLongTimeString());
+            }
+            else
+            {
+                await next.Invoke(context);
+            }
+        }
+    }
+    public static class TimeExtensions
+    {
+        public static IApplicationBuilder UseTime(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TimeMiddleware>();
+        }
+    }
+}
