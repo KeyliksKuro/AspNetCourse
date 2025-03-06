a31d7ffbac01e3f7f11e9141f2b138170f0473e7
KeyliksKuro <bsergey115@mail.ru>
2025-03-06 14:16:09 +0700

    full

---
+++ Lesson1/Examples/Example2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Routing.Patterns;
+using System;
+
+namespace Lesson1.Examples.Example2
+{
+
+    public class Example1 : IExample
+    {
+        public void Run()
+        {
+            var builder = WebApplication.CreateBuilder();
+            var app = builder.Build();
+
+            //Для параметров необходимо обязательно указывать тип данных
+
+            app.Map("/users/{id}", (int id) => $"User Id: {id}");
+
+            app.Map("/users/{id}/{name}",
+                (string id, string name) => $"User Id: {id}   User Name: {name}");
+
+            //Необязательные параметры
+            app.Map("/product/{id?}", (string? id) => $"User Id: {id ?? "Undefined"}");
+
+            //Значения параметров по умолчанию
+            app.Map("{controller=Home}/{action=Index}/{id?}",
+                (string controller, string action, string? id) =>
+                $"Controller: {controller} \nAction: {action} \nId: {id}");
+
+            app.Map("/users", () => "Users Page");
+            //app.Map("/", () => "Index Page");
+
+            app.Run();
+        }
+    }
+}
---
+++ Lesson1/Examples/Example5.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Routing.Patterns;
+using System;
+
+namespace Lesson1.Examples.Example5
+{
+    public class Example1 : IExample
+    {
+        public void Run()
+        {
+            var builder = WebApplication.CreateBuilder();
+            var app = builder.Build();
+
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine("First middleware starts");
+                await next.Invoke();
+                Console.WriteLine("First middleware ends");
+            });
+            app.Map("/", () =>
+            {
+                Console.WriteLine("Index endpoint starts and ends");
+                return "Index Page";
+            });
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine("Second middleware starts");
+                await next.Invoke();
+                Console.WriteLine("Second middleware ends");
+            });
+            app.Map("/about", () =>
+            {
+                Console.WriteLine("About endpoint starts and ends");
+                return "About Page";
+            });
+
+            //терминальный компонент будет выполняться даже если endpoint соответствует запрошенному пути
+
+            app.Run();
+        }
+
+    }
+}
