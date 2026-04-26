6744585cbf36782947cc6cea1fca3675e3bf10d3
Keyliks <bsergey115@mail.ru>
2026-04-26 13:21:42 +0700

    add refresh

--- AspNetCourse/Program.cs
+++ AspNetCourse/Program.cs
@@ -1,4 +1,4 @@
-using Application.Services.Abstraction;
+﻿using Application.Services.Abstraction;
 using Application.Services.Implementation;
 using Domain.Repositories;
 using Infastructre;
@@ -36,13 +36,56 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             {
                 context.Token = context.Request.Cookies["myToken"];
                 return Task.CompletedTask;
+            },
+            OnChallenge = async context =>
+            {
+                if (context.Handled) return;
+
+                // Пытаемся обновить токен с помощью refresh‑токена из cookie
+                var refreshToken = context.Request.Cookies["refresh_token"];
+                if (string.IsNullOrEmpty(refreshToken))
+                {
+                    context.HandleResponse();
+                    context.Response.Redirect("/Account/Login");
+                    return;
+                }
+
+                var dbContext = context.HttpContext.RequestServices.GetRequiredService<AppDbContext>();
+                var jwtService = context.HttpContext.RequestServices.GetRequiredService<IAuthService>();
+
+                var storedToken = await dbContext.RefreshTokens
+                    .Include(rt => rt.User)
+                    .FirstOrDefaultAsync(rt => rt.Token == refreshToken && !rt.IsRevoked && rt.ExpiresAt > DateTime.UtcNow);
+
+                if (storedToken == null)
+                {
+                    context.HandleResponse();
+                    context.Response.Redirect("/Account/Login");
+                    return;
+                }
+
+                var newAccessToken = jwtService.GenerateAccessToken(storedToken.User);
+
+                context.Response.Cookies.Append("access_token", newAccessToken, new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.Strict,
+                    Expires = DateTime.UtcNow.AddMinutes(jwtSettings.GetValue<int>("AccessTokenExpirationMinutes", 15))
+                });
+
+                // Заменяем токен в контексте аутентификации
+                context.Token = newAccessToken;
+                context.HandleResponse();
+
+                context.Response.Redirect(context.Request.Path + context.Request.QueryString);
             }
         };
     });
 builder.Services.AddAuthorization(options =>
     options.AddPolicy("NotForOlegs", builder =>
         builder.RequireAssertion(context =>
-            context.User.FindFirst(ClaimTypes.Name)?.Value != "����")));
+            context.User.FindFirst(ClaimTypes.Name)?.Value != "Олег")));
 
 var app = builder.Build();
 
